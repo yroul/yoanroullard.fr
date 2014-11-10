586083b32561dff183a968ebb8f3a2e1a6e2eb64
Yoan Roullard <yoan.roullard@gmail.com>
2014-11-10 12:46:35 +0100

    Add build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,82 @@
 module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+
+
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin:{
+      html:['dist/index.html']
+    },
+    copy:{
+      html: {
+        src: './index.html', dest: 'dist/index.html'
+      },
+      css: {
+        src: './css/custom.css', dest: 'dist/css/custom.css'
+      },
+      others : {
+        files : [{
+          expand: true,
+          src: [
+            './css/fonts/**',
+            './css/patterns/**',
+            './partials/**',
+          ],
+          dest: 'dist/'}
+        ]
+      }
+    },
+    concat: {
+      generated: {
+        files: [
+        {
+          dest: '.tmp/concat/js/app.js',
+          src: [
+          'js/*.js',
+          'bower_components/jquery/dist/jquery.min.js',
+          'bower_components/bootstrap/dist/js/bootstrap.min.js'
+          ]
+        }
+        ]
+      }
+    },
+    uglify: {
+      generated: {
+        files: [
+        {
+          dest: 'dist/js/app.js',
+          src: [ '.tmp/concat/js/app.js' ]
+        }
+        ]
+      }
+    },
+    clean: {
+      src : ['./dist','./.tmp']
+    }
+
   });
 
 
-  // Default task(s).
-//  grunt.registerTask('default', ['uglify']);
+
+  // simple build task
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'useminPrepare',
+    'concat',
+    'uglify',
+  //  'cssmin',
+    'usemin'
+  ]);
 
 };
